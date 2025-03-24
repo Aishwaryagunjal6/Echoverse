d6611422e6db061c0b9712d7bf3b4437604ba0b7
Aishwaryagunjal6 <gunjalaishwarya625@gmail.com>
2025-03-24 22:29:40 +0530

    first code setup

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const express= require('express')
-const app = express()
-const mongoose = require('mongoose')
+// const express= require('express')
+// const app = express()
+// const mongoose = require('mongoose')
 
-const PORT = process.env.PORT || 3000
+// const PORT = process.env.PORT || 3000
 
-mongoose.connect(process.env.MONGO_URL).then(()=>{
-  app.listen(PORT, ()=>{
-    console.log(`Server running on PORT ${PORT}`)
-  })
-}).catch((err)=>{
-  console.log(err)
-})
+// mongoose.connect(process.env.MONGO_URL).then(()=>{
+//   app.listen(PORT, ()=>{
+//     console.log(`Server running on PORT ${PORT}`)
+//   })
+// }).catch((err)=>{
+//   console.log(err)
+// })
 
