d00f7161d5e5d814bbc61960f466cc40c528ccef
Aishwaryagunjal6 <gunjalaishwarya625@gmail.com>
2025-03-25 21:35:28 +0530

    Models design and setup

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-// const express= require('express')
-// const app = express()
-// const mongoose = require('mongoose')
+const express= require('express')
+const app = express()
+const mongoose = require('mongoose')
+require ("dotenv").config()
 
-// const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000
 
-// mongoose.connect(process.env.MONGO_URL).then(()=>{
-//   app.listen(PORT, ()=>{
-//     console.log(`Server running on PORT ${PORT}`)
-//   })
-// }).catch((err)=>{
-//   console.log(err)
-// })
+mongoose.connect(process.env.MONGO_URL).then(()=>{
+  app.listen(PORT, ()=>{
+    console.log(`Server running on PORT ${PORT}`)
+  })
+}).catch((err)=>{
+  console.log(err)
+})
 
