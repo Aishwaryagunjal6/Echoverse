9711026f1f4b6278bdbec93337a924c3f45e383b
Aishwaryagunjal6 <gunjalaishwarya625@gmail.com>
2025-03-26 08:02:35 +0530

    routing changes

--- app.js
+++ app.js
@@ -5,6 +5,16 @@ require ("dotenv").config()
 
 const PORT = process.env.PORT || 3000
 
+app.set("view engine", "ejs");
+
+app.get("/auth/login",(req,res)=>{
+  res.render("login")
+})
+
+app.get("/auth/register",(req,res)=>{
+  res.render("register")
+})
+
 mongoose.connect(process.env.MONGO_URL).then(()=>{
   app.listen(PORT, ()=>{
     console.log(`Server running on PORT ${PORT}`)
